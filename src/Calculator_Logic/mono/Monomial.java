4d1aa4a80bde63a954363e0664571796ad9db935
avihay Abramov <avihay@avihays-Air.lan>
2025-04-14 08:45:12 -0700

    commit version 1.3

--- src/Calculator_Logic/mono/Monomial.java
+++ src/Calculator_Logic/mono/Monomial.java
@@ -1,12 +1,43 @@
 package Calculator_Logic.mono;
 
+import Calculator_Logic.poly.Polynomial;
+import Calculator_Logic.scalar.Scalar;
+
+import java.util.ArrayList;
+
 public class Monomial {
     //fields
+    private int exponent;
+    private Scalar coefficient;
 
     //constructors
 
     //getters and setters
 
     //methods
+    public Monomial add(Monomial monomial) {
+        return null;
+    }
+
+    public Monomial mul(Monomial monomial) {
+        return null;
+    }
+
+    public Scalar evaluate(Scalar scalar) {
+        return null;
+    }
+
+    public Monomial derivative() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    public String toString() {
+        return null;
+    }
 
 }
--- src/Calculator_Logic/poly/Polynomial.java
+++ src/Calculator_Logic/poly/Polynomial.java
@@ -1,12 +1,43 @@
 package Calculator_Logic.poly;
 
+import Calculator_Logic.mono.Monomial;
+import Calculator_Logic.scalar.Scalar;
+
+import java.util.ArrayList;
+
 public class Polynomial {
     //fields
+    private ArrayList<Monomial> monomials;
 
     //constructors
+
 
     //getters and setters
 
     //methods
+    public Polynomial add(Polynomial p) {
+        return null;
+    }
+
+    public Polynomial mul(Polynomial p) {
+        return null;
+    }
+
+    public Scalar evaluate(Scalar scalar) {
+        return null;
+    }
+
+    public Polynomial derivative() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    public String toString() {
+        return null;
+    }
 
 }
